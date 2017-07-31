25b92909f09ae6c8769447f1286e362c55828613
Pepe Valdivia <jvaldivia@softweb.pe>
2017-07-31 10:00:05 -0500

    Commit inicial

---
+++ Lib/Database.pm
@@ -0,0 +1,26 @@
+package Lib::Database;
+use MongoDB;
+use utf8;
+binmode STDOUT, ":encoding(utf8)";
+
+
+sub new {
+    my $class = shift;
+    my $userid = "";
+    my $password = "";
+    my $client = MongoDB->connect("mongodb://192.168.1.19");
+    my $db = $client->get_database("db_monta");
+    my $self = {
+        _db => $db
+    };
+
+    bless $self, $class;
+    return $self;
+}
+
+sub getConnection {
+    my( $self ) = @_;
+    return $self->{_db};
+}
+
+1;
---
+++ app.pl
@@ -0,0 +1,35 @@
+use Mojolicious::Lite;
+use Data::Dumper;
+use JSON;
+use JSON::XS 'decode_json';
+use Lib::Database;
+
+hook(before_dispatch => sub {
+	my $self = shift;
+
+	$self->res->headers->header('Access-Control-Allow-Origin'=> '*');
+	$self->res->headers->header('Access-Control-Allow-Credentials' => 'true');
+	$self->res->headers->header('Access-Control-Allow-Methods' => 'GET, OPTIONS, POST, DELETE, PUT');
+	$self->res->headers->header('Access-Control-Allow-Headers' => 'Content-Type, X-CSRF-Token');
+	$self->res->headers->header('x-powered-by' => 'Mojolicious (Perl)');
+	$self->res->headers->header('Access-Control-Max-Age' => '1728000');
+	#$self->respond_to(any => { data => '', status => 200 });
+});
+
+get '/' => sub {
+	my $c = shift;
+	my $db = 'Lib::Database'; my $odb= $db->new(); my $conn = $odb->getConnection();
+	my $docs = $conn->get_collection('criadores')->find();
+	my @rpta;
+   while (my $d = $docs->next) {
+       $d->{'_id'} = $d->{'_id'} . '';
+       push @rpta, $d;
+   }
+   my $json_text = to_json \@rpta;
+   #$self->render(text => ("$json_text"));
+  	$c->render(text => ("$json_text"));
+};
+
+app->start;
+
+#fuente : https://github.com/kraih/mojo/wiki/Writing-websocket-chat-using-Mojolicious-Lite
